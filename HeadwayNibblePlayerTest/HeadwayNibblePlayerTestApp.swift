3ded91bd9ade56a6e77dd86604c51be74718f7f0
effeto <effectdopler@gmail.com>
2024-03-27 13:24:13 +0200

    Initial Commit

---
+++ HeadwayNibblePlayerTest/HeadwayNibblePlayerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadwayNibblePlayerTestApp.swift
+//  HeadwayNibblePlayerTest
+//
+//  Created by Демьян on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct HeadwayNibblePlayerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
